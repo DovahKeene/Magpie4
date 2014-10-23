9c9418c785ea8bbe3d00817f14220e82804a0c58
Keene Jabbour <keenejabbour@yahoo.com>
2014-10-23 11:25:50 -0400

    Oct 23 11:25
    Almost done

--- src/Magpie4.java
+++ src/Magpie4.java
@@ -49,10 +49,19 @@ public class Magpie4
 		}
 
 		// Responses which require transformations
-		else if (findKeyword(statement, "I want to", 0) >= 0)
+            else if (findKeyword(statement, "I want", 0) >= 0)
 		{
 			response = transformIWantToStatement(statement);
 		}
+
+            else if (findKeyword(statement, "I", 0)>=0)
+                {
+                    if (findKeyword(statement, "you", 0)>=0)
+                    {
+                        String statement2 = statement;
+                        response = transformYouMeStatement(statement2);
+                    }
+                }
 
 		else
 		{
@@ -93,38 +102,36 @@ public class Magpie4
 		int psn = findKeyword (statement, "I want", 0);
 		String restOfStatement = statement.substring(psn + 6).trim();
 		return "Would you really be happy if you had " + restOfStatement + "?";
-	}
+
+    }
+
+    private String transformYouMeStatement (String statement)
+    {
+        statement = statement.trim();
+        String lastChar = statement.substring(statement
+                .length() - 1);
+        if (lastChar.equals("."))
+        {
+            statement = statement.substring(0, statement
+                    .length() - 1);
+        }
+
+        int psnOfYou = findKeyword (statement, "you", 0);
+        int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
+
+        String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
+        return "What makes you think that I " + restOfStatement + " you?";
+    }
 
 	
 	
 	/**
 	 * Take a statement with "you <something> me" and transform it into 
 	 * "What makes you think that I <something> you?"
-	 * @param statement the user statement, assumed to contain "you" followed by "me"
+	 * @param statement2 the user statement, assumed to contain "you" followed by "me"
 	 * @return the transformed statement
 	 */
-	private String transformYouMeStatement(String statement)
-	{
-		//  Remove the final period, if there is one
-		statement = statement.trim();
-		String lastChar = statement.substring(statement
-				.length() - 1);
-		if (lastChar.equals("."))
-		{
-			statement = statement.substring(0, statement
-					.length() - 1);
-		}
-		
-		int psnOfYou = findKeyword (statement, "you", 0);
-		int psnOfMe = findKeyword (statement, "me", psnOfYou + 3);
-		
-		String restOfStatement = statement.substring(psnOfYou + 3, psnOfMe).trim();
-		return "What makes you think that I " + restOfStatement + " you?";
-	}
-	
-	
 
-	
 	
 	/**
 	 * Search for one word in phrase.  The search is not case sensitive.
